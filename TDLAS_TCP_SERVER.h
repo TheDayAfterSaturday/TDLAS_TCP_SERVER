a6a90716495c1d9015f72f93b408b074ba0dbcf3
yangzhiyuan <yangzhiyuan0928@163.com>
2016-09-20 14:36:11 +0800

    整个上位机与下位机的联调基本调通，整个系统能正常使用了
    TDLAS_TCP_SERVER(09-13)工程：
    	本工程采用两个Buffer缓存数据，在Semphore机制下采用ping-pong操作完成数据读取和回写文本，遇到的问题是：Buffer2中数据可能无法回写到本地。
    TDLAS_TCP_SERVER(09-18)工程：
    	(1) 基本功能部分已调通。可能要求将检测数据部分和阈值部分的显示控件：Numeric Edit控件(Indicator Mode)换成String Edit控件(Indicator Mode)。我感觉没必要换。
    	(2) 可能存在Bebug的地方是多线程读写数据时，Semphore信号量的控制。

--- TDLAS_TCP_SERVER.h
+++ TDLAS_TCP_SERVER.h
@@ -15,76 +15,98 @@
      /* Panels and Controls: */
 
 #define  PANEL                            1       /* callback function: panelCB */
-#define  PANEL_QUIT                       2       /* control type: command, callback function: (none) */
-#define  PANEL_TAB                        3       /* control type: tab, callback function: (none) */
+#define  PANEL_QUIT                       2       /* control type: command, callback function: UnregisterServer */
+#define  PANEL_TAB                        3       /* control type: tab, callback function: GetLimitParam */
 #define  PANEL_TCP_LED                    4       /* control type: LED, callback function: (none) */
 #define  PANEL_TCP_CONFIG                 5       /* control type: command, callback function: Tcp_Config */
-#define  PANEL_TEC_MON_TIMER              6       /* control type: timer, callback function: Mon_timer */
+#define  PANEL_CH2_LED                    6       /* control type: LED, callback function: (none) */
+#define  PANEL_CH1_LED                    7       /* control type: LED, callback function: (none) */
 
      /* tab page panel controls */
-#define  TABPANEL_1_TEC_TEMP              2       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_1_TEMP_SET              3       /* control type: command, callback function: SetTemperature */
-#define  TABPANEL_1_READ_POWER_PARAM      4       /* control type: command, callback function: ReadPowerParam */
-#define  TABPANEL_1_NUMERIC_7             5       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_1_NUMERIC_6             6       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_1_NUMERIC_5             7       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_1_NUMERIC_3             8       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_1_NUMERIC_4             9       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_1_NUMERIC_2             10      /* control type: numeric, callback function: (none) */
-#define  TABPANEL_1_TEXTMSG_8             11      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_1_TEXTMSG               12      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_1_TEC_TEMP_MAX          2       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEC_TEMP_MIN          3       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEC_TEMP              4       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEMP_SET              5       /* control type: command, callback function: SetTemperature */
+#define  TABPANEL_1_LASER_LIMIT_I         6       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEC_REAL_I            7       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEC_REAL_V            8       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEC_CLIMIT_I          9       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEC_HLIMIT_I          10      /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEC_LIMIT_V           11      /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG_8             12      /* control type: textMsg, callback function: (none) */
 #define  TABPANEL_1_TEXTMSG_7             13      /* control type: textMsg, callback function: (none) */
 #define  TABPANEL_1_TEXTMSG_4             14      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_1_TEXTMSG_6             15      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_1_DECORATION            16      /* control type: deco, callback function: (none) */
-#define  TABPANEL_1_TEXTMSG_5             17      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_1_TEXTMSG_2             18      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_1_TEXTMSG_3             19      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_1_LASER_CTRL            20      /* control type: binary, callback function: Laser_Ctrl */
-#define  TABPANEL_1_TEC_CTRL              21      /* control type: binary, callback function: Tec_Ctrl */
-#define  TABPANEL_1_TEC_MON_CTRL          22      /* control type: binary, callback function: Tec_Mon_Ctrl */
-#define  TABPANEL_1_LASER_TEMP            23      /* control type: scale, callback function: (none) */
-#define  TABPANEL_1_LASER_CURRENT         24      /* control type: scale, callback function: (none) */
-#define  TABPANEL_1_TEMP_STRIP            25      /* control type: strip, callback function: (none) */
-#define  TABPANEL_1_CURRENT_STRIP         26      /* control type: strip, callback function: (none) */
+#define  TABPANEL_1_DECORATION            15      /* control type: deco, callback function: (none) */
+#define  TABPANEL_1_LASER_CTRL            16      /* control type: binary, callback function: Laser_Ctrl */
+#define  TABPANEL_1_TEC_CTRL              17      /* control type: binary, callback function: Tec_Ctrl */
+#define  TABPANEL_1_TEC_MON_CTRL          18      /* control type: binary, callback function: Tec_Mon_Ctrl */
+#define  TABPANEL_1_LASER_TEMP            19      /* control type: scale, callback function: (none) */
+#define  TABPANEL_1_LASER_CURRENT         20      /* control type: scale, callback function: (none) */
+#define  TABPANEL_1_TEMP_STRIP            21      /* control type: strip, callback function: (none) */
+#define  TABPANEL_1_LASER_POWER           22      /* control type: numeric, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG_9             23      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG_2             24      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG_3             25      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG_5             26      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_1_CURRENT_STRIP         27      /* control type: strip, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG_6             28      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG_12            29      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_1_DECORATION_2          30      /* control type: deco, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG_11            31      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG_10            32      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_1_DECORATION_3          33      /* control type: deco, callback function: (none) */
+#define  TABPANEL_1_TEXTMSG               34      /* control type: textMsg, callback function: (none) */
 
      /* tab page panel controls */
 #define  TABPANEL_2_PHASE_SQ2X            2       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_FREQ_SQ2X             3       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_PHASE_SQ1X            4       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_DELAY_SAW             5       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_DC_2                  6       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_DC_1                  7       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_AMP_SAW               8       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_FREQ_SAW              9       /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_AMP_SIN               10      /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_PHASE_SIN             11      /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_FREQ_SIN              12      /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_FREQ_SQ1X             13      /* control type: numeric, callback function: (none) */
-#define  TABPANEL_2_TEXTMSG               14      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_2_TEXTMSG_8             15      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_2_TEXTMSG_2             16      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_2_TEXTMSG_3             17      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_2_TEXTMSG_5             18      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_2_TEXTMSG_6             19      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_2_TEXTMSG_10            20      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_2_TEXTMSG_7             21      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_LASER_CURREBT_MAX     3       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_FREQ_SQ2X             4       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_PHASE_SQ1X            5       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_DELAY_SAW             6       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_DC_2                  7       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_DC_1                  8       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_AMP_SAW               9       /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_FREQ_SAW              10      /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_AMP_SIN               11      /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_PHASE_SIN             12      /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_FREQ_SIN              13      /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_FREQ_SQ1X             14      /* control type: numeric, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG               15      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_8             16      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_2             17      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_3             18      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_5             19      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_6             20      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_10            21      /* control type: textMsg, callback function: (none) */
 #define  TABPANEL_2_TEXTMSG_9             22      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_2_TEXTMSG_11            23      /* control type: textMsg, callback function: (none) */
-#define  TABPANEL_2_OUT_EN                24      /* control type: binary, callback function: (none) */
-#define  TABPANEL_2_DDS_CONFIG            25      /* control type: command, callback function: DDS_Config */
-#define  TABPANEL_2_TEXTMSG_12            26      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_OUT_EN                23      /* control type: binary, callback function: (none) */
+#define  TABPANEL_2_DDS_CONFIG            24      /* control type: command, callback function: DDS_Config */
+#define  TABPANEL_2_DECORATION            25      /* control type: deco, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_13            26      /* control type: textMsg, callback function: (none) */
 #define  TABPANEL_2_TEXTMSG_4             27      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_DECORATION_2          28      /* control type: deco, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_7             29      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_14            30      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_DECORATION_3          31      /* control type: deco, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_11            32      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_12            33      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_16            34      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_DECORATION_4          35      /* control type: deco, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_15            36      /* control type: textMsg, callback function: (none) */
+#define  TABPANEL_2_DECORATION_5          37      /* control type: deco, callback function: (none) */
+#define  TABPANEL_2_TEXTMSG_17            38      /* control type: textMsg, callback function: (none) */
 
      /* tab page panel controls */
-#define  TABPANEL_3_AD_CTRL               2       /* control type: binary, callback function: AD_Ctrl */
-#define  TABPANEL_3_AD_CH2_CTRL           3       /* control type: binary, callback function: (none) */
-#define  TABPANEL_3_AD_CH1_CTRL           4       /* control type: binary, callback function: (none) */
-#define  TABPANEL_3_AD_SAMPLE_LED         5       /* control type: LED, callback function: (none) */
+#define  TABPANEL_3_AD_CH2_CTRL           2       /* control type: binary, callback function: (none) */
+#define  TABPANEL_3_AD_CH1_CTRL           3       /* control type: binary, callback function: (none) */
+#define  TABPANEL_3_AD_LED_2              4       /* control type: LED, callback function: (none) */
+#define  TABPANEL_3_AD_LED_1              5       /* control type: LED, callback function: (none) */
 #define  TABPANEL_3_CH2_PATH              6       /* control type: string, callback function: (none) */
 #define  TABPANEL_3_BROWSER_CH2           7       /* control type: command, callback function: Browser_CH2 */
 #define  TABPANEL_3_CH1_PATH              8       /* control type: string, callback function: (none) */
 #define  TABPANEL_3_BROWSER_CH1           9       /* control type: command, callback function: Browser_CH1 */
+#define  TABPANEL_3_AD_CTRL_SET           10      /* control type: command, callback function: AD_Ctrl */
+#define  TABPANEL_3_SAMPLE_LED            11      /* control type: LED, callback function: (none) */
 
 
      /* Menu Bars, Menus, and Menu Items: */
@@ -98,14 +120,14 @@ int  CVICALLBACK AD_Ctrl(int panel, int control, int event, void *callbackData, 
 int  CVICALLBACK Browser_CH1(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Browser_CH2(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK DDS_Config(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK GetLimitParam(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Laser_Ctrl(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
-int  CVICALLBACK Mon_timer(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK panelCB(int panel, int event, void *callbackData, int eventData1, int eventData2);
-int  CVICALLBACK ReadPowerParam(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK SetTemperature(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Tcp_Config(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Tec_Ctrl(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 int  CVICALLBACK Tec_Mon_Ctrl(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
+int  CVICALLBACK UnregisterServer(int panel, int control, int event, void *callbackData, int eventData1, int eventData2);
 
 
 #ifdef __cplusplus
